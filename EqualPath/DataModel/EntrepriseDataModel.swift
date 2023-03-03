95f2749c6c5f3a56cc1b10a2fcaf706092844c3e
tageldin <maaztageldin030@hotmail.com>
2023-03-03 11:28:51 +0100

    company's logo

--- EqualPath/DataModel/EntrepriseDataModel.swift
+++ EqualPath/DataModel/EntrepriseDataModel.swift
@@ -13,6 +13,7 @@ struct Entreprises: Codable {
     var siret : String
     var createdAt: String
     var updatedAt: String
-    var grade: Int
+    var grade: Int?
     var buildings: [Batiment]
+    var imageUrl: String?
 }
